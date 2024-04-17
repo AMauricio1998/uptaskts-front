3b7f02085edd1bc6d66d448d5d8a9561ae4bf0fd
Alan Mauricio Reyes Telesforo <mauricio.gobti@gmail.com>
2024-04-16 18:14:29 -0600

    recuperacion de contraseña

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,13 @@ import DashboardView from "@/views/DashboardView";
 import CreateProjectView from "@/views/projects/CreateProjectView";
 import EditProjectView from "@/views/projects/EditProjectView";
 import ProjectDetailsView from "@/views/projects/ProjectDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
+import ConfirmAccountView from "./views/auth/ConfirmAccountView";
+import RequestNewCodeView from "./views/auth/RequestNewCodeView";
+import ForgotPasswordView from "./views/auth/ForgotPasswordView";
+import NewPasswordView from "./views/auth/NewPasswordView";
 
 export default function Router() {
 
@@ -15,6 +22,15 @@ export default function Router() {
                     <Route path="/projects/create" element={ <CreateProjectView />}/>
                     <Route path="/projects/:projectId" element={ <ProjectDetailsView />}/>
                     <Route path="/projects/:projectId/edit" element={ <EditProjectView />}/>
+                </Route>
+
+                <Route element={ <AuthLayout /> }>
+                    <Route index path="/auth/login" element={ <LoginView />}/>
+                    <Route index path="/auth/register" element={ <RegisterView />}/>
+                    <Route index path="/auth/confirm-account" element={ <ConfirmAccountView />}/>
+                    <Route index path="/auth/request-code" element={ <RequestNewCodeView />}/>
+                    <Route index path="/auth/forgot-password" element={ <ForgotPasswordView />}/>
+                    <Route index path="/auth/new-password" element={ <NewPasswordView />}/>
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,23 @@
 import { z } from "zod";
 
+/** AUTH USERS */
+const AuthSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+    password_confirmation: z.string(),
+    token: z.string()
+})
+
+export type Auth = z.infer<typeof AuthSchema>;
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>;
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>;
+export type RequestConfirmationCodeForm = Pick<Auth, 'email'>;
+export type ForgotPasswordForm = Pick<Auth, 'email'>;
+export type NewPasswordFormType = Pick<Auth, 'password' | 'password_confirmation'>;
+
+export type ConfirmToken = Pick<Auth, 'token'>;
+
 /** TASK */
 export const TaskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"]);
 export type TaskStatus = z.infer<typeof TaskStatusSchema>
